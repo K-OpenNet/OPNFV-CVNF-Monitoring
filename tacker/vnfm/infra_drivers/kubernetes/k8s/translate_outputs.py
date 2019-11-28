ffc6b3730b202a79b7178dbff27e368a3f5e3042
Jaewook Oh <kyle.oh95@gmail.com>
2019-11-28 02:43:52 +0000

    update

--- tacker/vnfm/infra_drivers/kubernetes/k8s/translate_outputs.py
+++ tacker/vnfm/infra_drivers/kubernetes/k8s/translate_outputs.py
@@ -220,7 +220,8 @@ class Transformer(object):
 
         # Create and configure a spec section
         pod_template = client.V1PodTemplateSpec(
-            metadata=client.V1ObjectMeta(labels=labels),
+            metadata=client.V1ObjectMeta(
+                labels=labels, annotations=tosca_kube_obj.annotations),
             spec=client.V1PodSpec(containers=containers))
 
         # Create the specification of deployment
